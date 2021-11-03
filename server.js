87e79a02bdccf86113244162dd55df8f75931055
prathamn007 <pratham.191mt034@nitk.edu.in>
2021-11-03 19:46:06 +0530

    fifth

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+/*unless we are in development environment, we dont want variables
+in .env to load*/
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+} 
+
 const express = require("express") //express app
 const app = express()
 const expressLayouts = require("express-ejs-layouts") //ejs layout
@@ -10,6 +16,18 @@ app.set('views', __dirname + '/views') //views directory
 app.set('layout', 'layouts/layout') //layouts file so that all html files need not be modified
 app.use(expressLayouts)
 app.use(express.static("public")) //includes public files such as html, css and js
+
+//connecting mongoose
+const mongoose = require('mongoose')
+//database connection is not hardcoded, so that both local and web connections to databases
+//are possible whenever needed
+mongoose.connect(process.env.DATABASE_URL, { useNewURLParser: true }) 
+//check whether we are connected or not
+const db = mongoose.connection
+//upon error
+db.on('error', error => console.error(error))
+//upon connection
+db.once('open', ()=>console.log('Connected to Mongoose')) 
 
 //use the router file
 app.use('/', indexRouter)
