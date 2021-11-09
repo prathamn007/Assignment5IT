efac77e4c3dfc3ee2f9b8da85dc08a239543e53e
prathamn007 <pratham.191mt034@nitk.edu.in>
2021-11-09 23:29:52 +0530

    finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,17 +1,34 @@
-//registering the plugins
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode
-)
+//getting the required styles property from the root CSS
+const rootStyles = window.getComputedStyle(document.documentElement)
 
-//changing the aspect ratio of the preview
-//resizig the image to be 150x100
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+//checking if main css is loaded
+if(rootStyles.getPropertyValue('--book-cover-width-large') != null && rootStyles.getPropertyValue('--book-cover-width-large') !== ''){
+    ready()
+}else{
+    document.getElementById('main-css')
+    .addEventListener('load', ready)
+}
 
-//transforming the files into filepond objects
-FilePond.parse(document.body);
+function ready(){
+    const coverWidth = parseFloat(rootStyles.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyles.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+    //registering the plugins
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode
+    )
+
+    //changing the aspect ratio of the preview
+    //resizig the image to be 150x100
+    FilePond.setOptions({
+        stylePanelAspectRatio: 1 / coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+
+    //transforming the files into filepond objects
+    FilePond.parse(document.body);
+}
+
